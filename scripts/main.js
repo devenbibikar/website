f58ebf27960cd9fb5d93074d410da746f8ba4c3e
devenbibikar <devenbibikar@gmail.com>
2023-05-23 19:53:35 -0500

    cleaned up javascript :

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,3 @@
-/*let myHeading = document.querySelector("h1");
-myHeading.textContent = "this webpage is currently under construction. stay tuned.";
-
-document.querySelector("h1").addEventListener("click", function () {
-    alert("this page has been attacked by viruses.")
-});
-
-//Instead of writing function() we can write () => 
-
-const myImage = document.querySelector("img");
-myImage.onclick = function () {
-const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/elizabethOfRussia.jpg") {
-    myImage.setAttribute("src", "images/chineseyoyo.jpeg");
-  } else {
-    myImage.setAttribute("src", "images/elizabethOfRussia.jpg");
-  }
-};
-
-let myButton = document.querySelector("button");
-
-function setUserName() {
-    const myName = prompt("Please enter your name.");
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Hello, ${myName}`;
-  }
-
-myButton.onclick = () => {
-    setUserName();
-};
-*/
-
-//NEW SECTION BEGINS
 
 let myNavigationItem = document.querySelectorAll(".navitem");
 myNavigationItem.forEach((item) => {
